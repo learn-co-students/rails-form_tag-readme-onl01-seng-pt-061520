34bda109be5495fcb2263c8cd71bb7052e063d68
CelinaJackson <celinanjackson@gmail.com>
2020-10-20 17:24:22 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title: 'Knitting Basics', description: 'Learn the basics of knitting')
