45da0d1a15ebeb20a4ced4580c26c46c26b4a0cd
hopegipson <gipson.hope@gmail.com>
2020-10-22 01:24:57 -0500

    Done.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+
+ 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -52,3 +54,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+
